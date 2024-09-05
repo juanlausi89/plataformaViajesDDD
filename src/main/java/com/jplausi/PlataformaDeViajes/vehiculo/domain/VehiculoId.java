ac023672a005a145d9f8236857d137fa1184dbf5
Juan Pablo Lausi <juanpablo_lausi@hotmail.com.ar>
2024-09-04 22:06:29 -0300

    value object vehiculeId

---
+++ src/main/java/com/jplausi/PlataformaDeViajes/vehiculo/domain/VehiculoId.java
@@ -0,0 +1,11 @@
+package com.jplausi.PlataformaDeViajes.vehiculo.domain;
+
+import com.jplausi.PlataformaDeViajes.shared.domain.Identifier;
+
+public final class VehiculoId extends Identifier {
+
+    public VehiculoId (String value){
+        super(value);
+    }
+    
+}
